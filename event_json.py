8e06712b6196d63335506f6efe76f5cadcc6ac94
Lawrence Chen <lawrence009@outlook.com>
2022-07-05 15:56:06 +0800

    single-line version

--- event_json.py
+++ event_json.py
@@ -9,18 +9,18 @@ addr_dir = (os.path.join(data_dir, each_subdir) for each_subdir in os.listdir(da
 addr_dir = (e for e in addr_dir if os.path.isdir(e))
 
 # get only .json from each directory, sort the file numerically, and
-# combine them into one multi-line .json
+# combine them into one single-line .json
 for each_dir in addr_dir:
-    ix = [int(each_json[:-5]) for each_json in os.listdir(each_dir) if re.search('\.json', each_json)]
-    ix.sort()
+    ix = sorted([int(each_json[:-5]) for each_json in os.listdir(each_dir) if re.search('\.json', each_json)])
     fn = (os.path.join(each_dir, f'{i}.json') for i in ix)
 
-    asset_events = []
-    for each_json in fn:
-        with open(each_json, 'r') as f:
-            resp = json.load(f)
-            asset_events.extend(resp['asset_events'])
-    with open(os.path.join(data_dir, os.path.basename(each_dir) + '.json'), 'w') as f:
-        json.dump(asset_events, fp=f)
+    i = 0
+    with open(os.path.join(data_dir, os.path.basename(each_dir) + '.json'), 'w', encoding='utf8') as fw:
+        for each_json in fn:
+            with open(each_json, 'r') as fr:
+                resp = json.load(fr)
+            for e in resp['asset_events']:
+                fw.write(str(e) + '\n')
+                i += 1
 
-    print(len(asset_events))
+    print(f'{i}, {os.path.basename(each_dir)}')
