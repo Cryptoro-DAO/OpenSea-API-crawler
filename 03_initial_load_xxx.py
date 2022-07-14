c53d9cf32b812672d9d0af894e6986e1e4e93ebc
lawrence009@outlook.com <lawrence009@outlook.com>
2022-07-14 22:16:56 +0000

    merge Cool Cats

--- 03_initial_load_xxx.py
+++ 03_initial_load_xxx.py
@@ -1,11 +1,12 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Initial loading of Clone X and Doodles into `opensea_events` delta table
+# MAGIC # Initial loading of Clone X, Doodles and Cool Cats into `opensea_events` delta table
 # MAGIC 
 # MAGIC Collection |asset contract address
 # MAGIC -|-
 # MAGIC Clone X | 0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B  
 # MAGIC Doodles | 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e
+# MAGIC Cool Cats | 0x1A92f7381B9F03921564a437210bB9396471050C
 
 # COMMAND ----------
 
@@ -13,12 +14,9 @@
 
 # COMMAND ----------
 
-# MAGIC %ls /tmp/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B
-
-# COMMAND ----------
-
-# MAGIC %sh
-# MAGIC ls /tmp/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B | wc -l
+# MAGIC %md # Clone X
+# MAGIC 
+# MAGIC Unzipping and moving json to temp folder for ingestion
 
 # COMMAND ----------
 
@@ -32,11 +30,7 @@
 
 # COMMAND ----------
 
-# MAGIC %sh mv /dbfs/mnt/opensea/asset_events/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B.zip /dbfs/mnt/opensea-sg/imported/asset_events/asset_contract_address/
-
-# COMMAND ----------
-
-# MAGIC %fs ls /dbfs/mnt/opensea-sg/tmp/asset_contract_address
+# MAGIC %ls /tmp/asset_contract_address/0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e | wc -l
 
 # COMMAND ----------
 
@@ -111,16 +105,119 @@ display(deltaTblEvents.history())
 
 # COMMAND ----------
 
-# clean up after imported
-# %sh rm -r /dbfs/mnt/opensea-sg/tmp/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B
+# MAGIC %md clean up temp dir after ingested
 
+# COMMAND ----------
+
+# MAGIC %fs mv /mnt/opensea/asset_events/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B.zip /mnt/opensea-sg/imported/asset_events/asset_contract_address
+
+# COMMAND ----------
+
+# MAGIC %sh rm -r /dbfs/mnt/opensea-sg/tmp/asset_contract_address/0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B
+
+# COMMAND ----------
+
+# MAGIC %md Moving ZIP file to archival bucket, i.e. imported
+
+# COMMAND ----------
+
+# MAGIC %ls /dbfs/mnt/opensea-sg/imported/asset_events/asset_contract_address
+
+# COMMAND ----------
+
+# MAGIC %md # Doodles
+# MAGIC 
+# MAGIC 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e
+# MAGIC 
+# MAGIC Unzip
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC unzip -uq /dbfs/mnt/opensea/asset_events/asset_contract_address/0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e.zip -d /dbfs/tmp/asset_contract_address
+# MAGIC mv -r /tmp/asset_contract_address /dbfs/mnt/opensea-sg/
+
+# COMMAND ----------
+
+base_location = '/mnt/opensea-sg/tmp/asset_contract_address/asset_contract_address/0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e/'
+# df = spark.read.option('recursiveFileLookup', 'true').json(path=base_location, schema=schema)
+df = spark.read.option('recursiveFileLookup', 'true').json(path=base_location)
+
+# COMMAND ----------
+
+from delta.tables import *
+from pyspark.sql.functions import explode
+
+deltaTblEvents = DeltaTable.forName(spark, 'opensea_events')
+
+dfUpdates = df.select(explode(df.asset_events).alias('asset_event')) \
+    .select('asset_event.*')
+
+deltaTblEvents.alias('events') \
+  .merge(
+    dfUpdates.alias('updates'),
+    'events.id = updates.id'
+  ) \
+  .whenNotMatchedInsertAll() \
+  .execute()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select collection_slug, count(event_type) from opensea_events group by collection_slug
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESCRIBE HISTORY default.opensea_events
+
+# COMMAND ----------
+
+# MAGIC %md _todo:_ finish cleaning up after I get back ...
+
+# COMMAND ----------
+
+# MAGIC %md # Cool Cats
+# MAGIC 0x1A92f7381B9F03921564a437210bB9396471050C
+
+# COMMAND ----------
+
+# MAGIC %ls -l /dbfs/mnt/opensea-sg/lz/asset_events/asset_contract_address
+
+# COMMAND ----------
+
+base_location = '/mnt/opensea-sg/lz/asset_events/asset_contract_address/0x1A92f7381B9F03921564a437210bB9396471050C'
+# df = spark.read.option('recursiveFileLookup', 'true').json(path=base_location, schema=schema)
+df = spark.read.option('recursiveFileLookup', 'true').json(path=base_location)
+
+# COMMAND ----------
+
+from delta.tables import *
+from pyspark.sql.functions import explode
+
+deltaTblEvents = DeltaTable.forName(spark, 'opensea_events')
+
+dfUpdates = df.select(explode(df.asset_events).alias('asset_event')) \
+    .select('asset_event.*')
+
+deltaTblEvents.alias('events') \
+  .merge(
+    dfUpdates.alias('updates'),
+    'events.id = updates.id'
+  ) \
+  .whenNotMatchedInsertAll() \
+  .execute()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select collection_slug, count(event_type) from opensea_events group by collection_slug
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESCRIBE HISTORY default.opensea_events
 
 # COMMAND ----------
 
 
-
-# COMMAND ----------
-
-# %sh
-# unzip -uq /dbfs/mnt/opensea/asset_events/asset_contract_address/0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e.zip -d /dbfs/tmp/asset_contract_address
-# mv -r /tmp/asset_contract_address /dbfs/mnt/opensea-sg/tmp/asset_contract_address
