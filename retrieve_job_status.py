da682f9e0f3d4cdca74674820634ebb087348286
Lawrence Chen <lawrence009@outlook.com>
2022-07-20 14:59:55 +0800

    retrieve and update job status in memory

--- retrieve_job_status.py
+++ retrieve_job_status.py
@@ -1,7 +1,7 @@
 """
-Procedure to read and update job status, where jobs are event-retrieving tasks
+Procedure to retrieve and update job status, which jobs are event-retrieving tasks
 The output of the tasks are gzip json files.
-The procedure reads the 2nd and the last file retrieved to update the page_num index and the cursor value
+The procedure reads the 2nd and the last json retrieved to update the page_num index and the cursor value
 """
 import gzip
 import json
@@ -10,29 +10,42 @@ import pandas as pd
 import s3fs
 
 
-jobs = pd.read_csv(os.path.join(os.getcwd(), 'jobs.csv'))
+jobs_nxt = pd.read_csv(os.path.join(os.getcwd(), 'jobs.csv'))
+jobs_prv = pd.read_csv(os.path.join(os.getcwd(), 'jobs_get_previous.csv'))
 
 base_uri = 'opensea-sg/lz/asset_events/20220719/asset_contract_address/'
 # base_uri = 'opensea-sg/lz/asset_events/asc-20220718T0947cst/' \
 #            'asset_contract_address/0x23581767a106ae21c074b2276D25e5C3e136a68b/'
 fs = s3fs.S3FileSystem(anon=False)
-asset_contract_addr = fs.ls(base_uri)
-for _addr in asset_contract_addr:
-    if fs.isdir(_addr):
-        uri = [path for path in fs.ls(_addr)]
-        print(len(uri))
-        # sort URI
-        i = [int(os.path.basename(ea)[:-len('.json.gz')]) for ea in uri]
+ls_uri = fs.ls(base_uri)
+for _uri in ls_uri:
+    if fs.isdir(_uri):
+        obj = [path for path in fs.ls(_uri)]
+        print('number of objs:', len(obj))
+        # sort objects: 1.json.gz, 10.json.gz, 2.json.gz, 3.json.gz...
+        i = [int(os.path.basename(ea)[:-len('.json.gz')]) for ea in obj]
         i.sort()
-        head_2 = f'{_addr}/{i[1]}.json.gz'  # 2nd item from the head
-        tail = f'{_addr}/{i[-1]}.json.gz'
-        print(head_2, tail)
+        head = f'{_uri}/{i[1]}.json.gz'  # 2nd item from the head
+        tail = f'{_uri}/{i[-1]}.json.gz'
+        print('head:{}\ntail:{}'.format(head, tail))
 
-        with fs.open(head_2, 'rb') as fread:
+        asset_contract_address = os.path.basename(_uri)
+        i = jobs_nxt.asset_contract_address == asset_contract_address
+
+        # head = get previous
+        with fs.open(head, 'rb') as fread:
             with gzip.open(fread) as gz:
                 events = json.load(gz)
-                print(os.path.basename(head_2), events['next'], events['previous'])
+        jobs_prv.loc[i, 'cursor'] = events['previous']
+
+        # tail = get next
+        with fs.open(tail, 'rb') as fread:
+            with gzip.open(fread) as gz:
+                events = json.load(gz)
+        jobs_nxt.loc[i, ['cursor', 'page_num']] = [events['next'], int(os.path.basename(tail)[:-len('.json.gz')]) + 1]
 
     else:
-        _bucket, _path, _ver_id = fs.split_path(_addr)
+        _bucket, _path, _ver_id = fs.split_path(_uri)
         print(os.path.basename(_path)[:-len('.json.gz')])
+
+print('done')
